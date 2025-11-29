18fef50b01abefa823df2f34dc43f28eeca1d46a
Samiam-wq <usamaiftikhar1@hotmail.com>
2025-11-29 18:19:22 -0500

    Add files via upload
    Servlet component of Book Inventory Management system by Usama Iftikhar. Added Relevant Book and BookCatalouge classes to Model layer as .txt files. (Text file format to avoid conflict with similarly names .java classes.)

---
+++ src/main/java/com/ac/cst8319/lms/service/BookServlet.java
@@ -0,0 +1,94 @@
+package com.library.controller;
+
+import com.library.model.Book;
+import com.library.model.BookCatalogue;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+// Maps this servlet to the URL pattern "/BookServlet"
+@WebServlet(name = "BookServlet", value = "/BookServlet")
+public class BookServlet extends HttpServlet {
+    // Use the BookCatalogue as an in-memory "database"
+    private BookCatalogue catalogue = new BookCatalogue();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+
+        if (action == null) {
+            action = "list"; // Default action
+        }
+
+        switch (action) {
+            case "list":
+                listBooks(request, response);
+                break;
+            case "editForm":
+                showEditForm(request, response);
+                break;
+            case "delete":
+                deleteBook(request, response);
+                break;
+            default:
+                listBooks(request, response);
+                break;
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if ("add".equals(action)) {
+            addBook(request, response);
+        } else if ("edit".equals(action)) {
+            updateBook(request, response);
+        }
+    }
+
+    private void listBooks(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Book> bookList = catalogue.getBooks();
+        request.setAttribute("bookList", bookList);
+        // Forward to the JSP page to display the list
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/inventoryList.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void addBook(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Simple parameter parsing
+        int id = Integer.parseInt(request.getParameter("id"));
+        String title = request.getParameter("title");
+        String author = request.getParameter("author");
+        boolean isAvailable = Boolean.parseBoolean(request.getParameter("available"));
+
+        Book newBook = new Book(id, title, author, isAvailable);
+        catalogue.addBook(newBook);
+        response.sendRedirect("BookServlet?action=list"); // Redirect back to list view
+    }
+    
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Book existingBook = catalogue.findBookById(id);
+        request.setAttribute("book", existingBook);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/editBookForm.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void updateBook(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String title = request.getParameter("title");
+        String author = request.getParameter("author");
+        boolean isAvailable = Boolean.parseBoolean(request.getParameter("available"));
+        
+        Book updatedBook = new Book(id, title, author, isAvailable);
+        catalogue.updateBook(updatedBook);
+        response.sendRedirect("BookServlet?action=list");
+    }
+
+    private void deleteBook(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        catalogue.deleteBook(id);
+        response.sendRedirect("BookServlet?action=list");
+    }
+}
